b877f85211801c3531e32391adae9b2cc7dd5580
galiyev <kairat.galyev@gmail.com>
2021-09-07 18:49:19 +0600

    COM DLL 50%

--- RFIDCOM/IMyRFIDReader.cs
+++ RFIDCOM/IMyRFIDReader.cs
@@ -3,28 +3,28 @@
 namespace RFIDCOM
 {
     [ComVisible(true)]
-    [Guid("6F0E241A-8575-44BF-B237-2B1913ABD102")]
+    [Guid("8F0E241A-8575-44BF-B237-2B1913ABD102")]
     [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
-
     internal interface IMyRFIDReader
     {
         [DispId(1)]
-        //Установка значений
-        string Set(string IP, int port, int timeOut);
+        //4. описываем методы которые можно будет вызывать из вне
+        void TagReaded();
 
         [DispId(2)]
         //4. описываем методы которые можно будет вызывать из вне
         void Start();
 
-
         [DispId(3)]
         //4. описываем методы которые можно будет вызывать из вне
         void Stop();
 
-        [DispId(4)]
-        //4. описываем методы которые можно будет вызывать из вне
-        void TagReaded();
 
+        [DispId(4)]
+        //Установка значений
+        string Set(string IP, int port, int timeOut);
+
+        
         [DispId(5)]
         //4. описываем методы которые можно будет вызывать из вне
         void SendEvent();
--- RFIDCOM/MyRFID2.cs
+++ RFIDCOM/MyRFID2.cs
@@ -9,12 +9,13 @@ using System.Windows.Forms;
 namespace RFIDCOM
 {
     [ComVisible(true)]
-    [Guid("212D1429-D278-439F-8CCA-4968DA90122C"), ClassInterface(ClassInterfaceType.AutoDual),
-     ComSourceInterfaces(typeof(IMyRFIDReader))]
-    [ProgId("RFIDCOM.MyRFID2")]
+    [Guid("212D1429-D278-439F-8CCA-4968DA90132C")]
+    [ComSourceInterfaces(typeof(IMyRFIDReader))]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    //[ProgId("RFIDCOM.MyRFID2")]
     public class MyRFID2
     {
-        public delegate void ReadingDelegate(string значения);
+        public delegate void ReadingDelegate(object значения);
         
         public event ReadingDelegate TagReaded;
 
@@ -28,15 +29,16 @@ namespace RFIDCOM
             MessageBox.Show("Stopped");
         }
 
+     
+        public void Set(string IP, int port, int timeOut)
+        {
+            MessageBox.Show($"IP: {IP} port: {port} timeout:{timeOut}");
+        }
+        
         public void SendEvent()
         {
             TriggerReading();
             MessageBox.Show("Trigger");
-        }
-
-        public void Set(string IP, int port, int timeOut)
-        {
-            MessageBox.Show($"IP: {IP} port: {port} timeout:{timeOut}");
         }
 
         public void TriggerReading()
